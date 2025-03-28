c52618f6064213d51b95f60e4c1962e995a5f94b
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-28 21:52:16 +0700

    Update Pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
-import type { NextPage } from "next";
-import React from "react";
+import { NextPage } from "next";
+import React, { useMemo } from "react";
 import Image from "next/image";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
 
-const loremParagraphs = new Array(30).fill(
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, alias. Amet ea neque autem mollitia facere inventore rerum deserunt, laborum enim esse vel beatae vitae, iusto eaque sit a sunt."
-);
-
 const Home: NextPage = () => {
+  const loremParagraphs = useMemo(
+    () =>
+      Array.from({ length: 30 }, () =>
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, alias. Amet ea neque autem mollitia facere inventore rerum deserunt, laborum enim esse vel beatae vitae, iusto eaque sit a sunt."
+      ),
+    []
+  );
+
   return (
     <Layout pageTitle="Home Page">
-      <Image src="/heloo.jpg" width={200} height={200} alt="Greeting illustration" />
-      <p className={styles["title-homepage"]}>Heloo</p>
-      <div className="text-container">
+      <Image src="/heloo.jpg" width={200} height={200} alt="Illustration of a greeting" />
+      <p className={styles.titleHomepage}>Heloo</p>
+      <div className={styles.textContainer}>
         {loremParagraphs.map((text, index) => (
           <p key={index}>{text}</p>
         ))}
