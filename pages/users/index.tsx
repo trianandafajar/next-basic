edf840313e882b097ec0aecd75789f70bb9a3d4f
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-28 21:52:36 +0700

    Update Pages Users

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,37 +1,55 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import styles from "../../styles/Users.module.css";
 
-interface UsersProps {
-  dataUsers: Array<any>;
+interface User {
+  id: number;
+  name: string;
+  email: string;
 }
 
-export default function Users(props: UsersProps) {
-  const { dataUsers } = props;
+interface UsersProps {
+  dataUsers: User[];
+}
+
+const Users: React.FC<UsersProps> = ({ dataUsers }) => {
   const router = useRouter();
+
+  const handleClick = useCallback(
+    (id: number) => {
+      router.push(`/users/${id}`);
+    },
+    [router]
+  );
+
   return (
     <Layout pageTitle="Users Page">
-      {dataUsers.map((user) => (
-        <div
-          key={user.id}
-          onClick={() => router.push(`/users/${user.id}`)}
-          className={styles.card}
-        >
-          <p>{user.name}</p>
-          <p>{user.email}</p>
-        </div>
-      ))}
+      <div className={styles.userContainer}>
+        {dataUsers.map((user) => (
+          <div
+            key={user.id}
+            onClick={() => handleClick(user.id)}
+            className={styles.card}
+          >
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </div>
     </Layout>
   );
-}
+};
 
 export async function getStaticProps() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const dataUsers = await res.json();
+  const dataUsers: User[] = await res.json();
+
   return {
     props: {
       dataUsers,
     },
   };
 }
+
+export default Users;
